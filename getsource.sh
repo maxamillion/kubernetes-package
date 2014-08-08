fb9f98c9b7dd8f7d7930f3b7745460483a1c593e
Eric Paris <eparis@redhat.com>
2014-08-08 16:50:19 -0400

    make getsource auto update the spec file

--- getsource.sh
+++ getsource.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
 wget https://github.com/GoogleCloudPlatform/kubernetes/archive/$1/kubernetes-${1:0:8}.tar.gz
+
+#put the git hash in there
+sed -i -e "s/%global commit\t\t[[:xdigit:]]\{40\}/%global commit\t\t$1/" kubernetes.spec
+
+#increment the version number
+VAL=`sed -n -e "s/Release:\t[[:digit:]]*.[[:digit:]]*.\([[:digit:]]*\).git.*/\1/p" kubernetes.spec`
+VAL=$((VAL + 1))
+sed -i -e "s/\(Release:\t[[:digit:]]*.[[:digit:]]*.\)\([[:digit:]]*\)\(.git\)/\1${VAL}\3/" kubernetes.spec
