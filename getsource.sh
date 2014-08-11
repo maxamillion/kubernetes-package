24e015bb6a7999088749b611e84ee1834c3672d7
Adam Miller <admiller@redhat.com>
2014-08-11 10:45:33 -0400

    Updates from admiller to decouple all third party libs

--- getsource.sh
+++ getsource.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm kubernetes-*.tar.gz
+
 wget https://github.com/GoogleCloudPlatform/kubernetes/archive/$1/kubernetes-${1:0:8}.tar.gz
 
 #put the git hash in there
@@ -9,3 +11,5 @@ sed -i -e "s/%global commit\t\t[[:xdigit:]]\{40\}/%global commit\t\t$1/" kuberne
 VAL=`sed -n -e "s/Release:\t[[:digit:]]*.[[:digit:]]*.\([[:digit:]]*\).git.*/\1/p" kubernetes.spec`
 VAL=$((VAL + 1))
 sed -i -e "s/\(Release:\t[[:digit:]]*.[[:digit:]]*.\)\([[:digit:]]*\)\(.git\)/\1${VAL}\3/" kubernetes.spec
+
+sha256sum kubernetes-*.tar.gz kubernetes.spec > sources
